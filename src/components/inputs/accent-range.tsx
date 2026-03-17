da6d3546252c304761960044ed3023517c2724bb
Danny Nguyen <danny@keeb.io>
2026-03-17 16:30:33 -0400

    Revert "Add slider value to the left of the slider for lighting control"
    This reverts commit d748e72dacc8bc9850a51bf8597b1ed2d720aca8.

--- src/components/inputs/accent-range.tsx
+++ src/components/inputs/accent-range.tsx
@@ -1,22 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.span`
-  display: inline-flex;
+  display: inline-block;
   line-height: initial;
-  width: 250px;
-  gap: 8px;
+  width: 200px;
 `;
 
-const ValueLabel = styled.span`
-  min-width: 2.5em;
-  text-align: right;
-  font-size: 0.875em;
-  color: var(--color_label-highlighted);
-  flex-shrink: 0;
-`;
-
-const SliderInput = styled.input.attrs({ type: 'range' }) <any>`
+const SliderInput = styled.input.attrs({type: 'range'})<any>`
   accent-color: var(--color_accent);
   width: 100%;
 `;
@@ -25,19 +16,13 @@ export const AccentRange: React.FC<
   Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'> & {
     onChange: (x: number) => void;
   }
-> = (props) => {
-  const [value, setValue] = useState<number>(+(props.defaultValue ?? 0));
-  return (
-    <Container>
-      <ValueLabel>{value}</ValueLabel>
-      <SliderInput
-        {...props}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-          const num = +e.target.value;
-          setValue(num);
-          props.onChange && props.onChange(num);
-        }}
-      />
-    </Container>
-  );
-};
+> = (props) => (
+  <Container>
+    <SliderInput
+      {...props}
+      onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+        props.onChange && props.onChange(+e.target.value);
+      }}
+    />
+  </Container>
+);

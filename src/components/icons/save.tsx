dbbbf6318c4660d7684895fdc05e750df523df86
Danny Nguyen <danny@keeb.io>
2026-06-19 16:53:14 -0400

    Rename Save to Export and Load to Import do clear up confusion

--- src/components/icons/save.tsx
+++ src/components/icons/save.tsx
@@ -1,5 +1,5 @@
 import {faFloppyDisk} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
-export const title = 'Save + Load';
+export const title = 'Export/Import';
 export const component = () => <FontAwesomeIcon icon={faFloppyDisk} />;
